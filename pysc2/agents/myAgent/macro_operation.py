6c6f610e112f535e9422339c51e3c00d7d1442c2
Hotpotfish <13281040@bjtu.edu.cn>
2019-11-02 20:49:27 +0800

    试上传

---
+++ pysc2/agents/myAgent/macro_operation.py
@@ -0,0 +1,314 @@
+import random
+from pysc2.lib import actions
+from pysc2.lib import features
+from pysc2.lib import units
+from queue import Queue
+import numpy as np
+
+
+_NOT_QUEUED = [0]
+_QUEUED = [1]
+
+screenSize = 256
+minimapSize = 64
+
+
+def ownMineralFields(MineralFields, CommandCenter):
+
+    if len(CommandCenter) and len(MineralFields):
+        MineralFields = np.array(MineralFields)
+        CommandCenter = np.array(CommandCenter)
+        for i in range(0, len(CommandCenter)):
+            Command = np.array([CommandCenter[i].x, CommandCenter[i].y])
+            for j in range(0, len(MineralFields)):
+                Mineral = np.array([MineralFields[j].x, MineralFields[j].y])
+
+                if np.sqrt(np.sum(np.square(Command - Mineral))) <= 30:
+                    return Mineral
+
+    else:
+
+        return None
+
+
+def findAnyEnemies(obs):
+    enemy_y, enemy_x = (obs.observation['feature_screen'][
+                            features.SCREEN_FEATURES.player_relative.index] == features.PlayerRelative.ENEMY).nonzero()
+    enemies = list(zip(enemy_x, enemy_y))
+
+    if enemies:
+        target = enemies[np.argmax(np.array(enemies)[:, 1])]
+        return target
+
+    else:
+
+        return None
+
+
+
+def needFood(obs):
+    if obs.observation['player'][4] != 200 and obs.observation['player'][4] - obs.observation['player'][3] <= 0:
+        return buildSupplydepot(obs)
+    else:
+        return None
+
+
+def chooseARandomPlace(input_x, input_y):
+    add_y = random.randint(-10, 10)
+    add_x = random.randint(-10, 10)
+
+    if input_x + add_x >= screenSize:
+
+        outx = screenSize
+
+    elif input_x + add_x < 0:
+        outx = 0
+
+    else:
+        outx = input_x + add_x
+
+    if input_y + add_y >= screenSize:
+
+        outy = screenSize
+
+    elif input_y + add_y < 0:
+        outy = 0
+
+    else:
+        outy = input_y + add_y
+
+    return [outx, outy]
+
+
+def searchUnitScreen(obs, unitType, alliance):
+    # feature_units = obs.observation['feature_units']
+    # examples = []
+    #
+    # for i in range(0, len(feature_units)):
+    #     if feature_units[i][0] == int(unitType) and feature_units[i][1] == alliance:
+    #         examples.append(feature_units[i])
+    #
+    # return examples
+    class point:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+    feature_screen = obs.observation['feature_screen']
+    examples = []
+
+    units_y, units_x = (feature_screen[features.SCREEN_FEATURES.unit_type.index] == int(unitType)).nonzero()
+
+    if units_y is not None:
+        for i in range(0, len(units_y)):
+            if feature_screen[features.SCREEN_FEATURES.player_relative.index][units_y[i]][units_x[i]] == alliance:
+                examples.append(point(units_x[i], units_y[i]))
+
+    if examples is not None:
+        return examples
+    else:
+        return None
+
+
+def noOP():
+    actionQueue = Queue()
+    actionQueue.put(actions.FunctionCall(actions.FUNCTIONS.no_op.id, []))
+    return actionQueue
+
+
+def svcBackToWork(obs, step):
+    # print('svcBackToWork')
+
+    MineralFields = searchUnitScreen(obs, units.Neutral.MineralField, 3)
+    MineralField750s = searchUnitScreen(obs, units.Neutral.MineralField750, 3)
+    CommandCenter = searchUnitScreen(obs, units.Terran.CommandCenter, 1)
+
+    if obs.observation['player'][7] != 0 and (len(MineralFields) or len(MineralField750s)):
+        # 2代表选择所有空闲工人
+        if step == 0:
+            return actions.FunctionCall(actions.FUNCTIONS.select_idle_worker.id, [[2]])
+
+        if step == 1:
+
+            if MineralFields is not None:
+                target = ownMineralFields(MineralFields, CommandCenter)
+                if target is not None:
+                    # ran = random.randint(0, len(MineralFields) - 1)
+                    # target = [MineralFields[ran].x, MineralFields[ran].y]
+                    return actions.FunctionCall(actions.FUNCTIONS.Harvest_Gather_screen.id, [_QUEUED, target])
+
+            if MineralField750s is not None:
+                target = ownMineralFields(MineralField750s, CommandCenter)
+                if target is not None:
+                    # ran = random.randint(0, len(MineralField750s) - 1)
+                    # target = [MineralField750s[ran].x, MineralField750s[ran].y]
+                    return actions.FunctionCall(actions.FUNCTIONS.Harvest_Gather_screen.id, [_QUEUED, target])
+
+        else:
+            return None
+
+
+
+
+    else:
+
+        return None
+
+
+def moveCarmeraToPlayerSelf_atomicOp(obs):
+    playerRelative = obs.observation['feature_minimap'][features.MINIMAP_FEATURES.player_relative.index]
+    pr_y, pr_x = (playerRelative == features.PlayerRelative.SELF).nonzero()
+
+    ran = random.randint(0, len(pr_y) - 1)
+
+    target = [pr_x[ran], pr_y[ran]]
+
+    return actions.FunctionCall(actions.FUNCTIONS.move_camera.id, [target])
+
+
+def chooseARandomScv_atomicOp(obs):
+    # 找寻空闲工人
+    # print('chooseARandomScv_atomicOp')
+    if obs.observation['player'][7] != 0:
+        return actions.FunctionCall(actions.FUNCTIONS.select_idle_worker.id, [[0]])
+
+    SCVs = searchUnitScreen(obs, units.Terran.SCV, 1)
+
+    if len(SCVs):
+        ran = random.randint(0, len(SCVs) - 1)
+        target = [SCVs[ran].x, SCVs[ran].y]
+
+        return actions.FunctionCall(actions.FUNCTIONS.select_point.id, [[0], target])
+
+    return None
+
+
+def chooseARandomScv(obs, step):
+    # actionQueue.put(moveCarmeraToPlayerSelf_atomicOp(obs))
+
+    SCVs = searchUnitScreen(obs, units.Terran.SCV, 1)
+
+    if len(SCVs):
+        i = random.randint(0, len(SCVs) - 1)
+        target = [SCVs[i].x, SCVs[i].y]
+        if step == 0:
+            # 这里的0代表选择一个的意思
+            return actions.FunctionCall(actions.FUNCTIONS.select_point.id, [[0], target])
+    return None
+
+
+def buildBarracks(obs, step):
+    # print('buildBarracks')
+
+    # actionQueue.put(moveCarmeraToPlayerSelf_atomicOp(obs))
+
+    if not chooseARandomScv_atomicOp(obs):
+        return None
+
+    if step == 0:
+        return chooseARandomScv_atomicOp(obs)
+
+    supplyDepots = searchUnitScreen(obs, units.Terran.SupplyDepot, 1)
+    #Barracks = searchUnitScreen(obs, units.Terran.Barracks, 1)
+
+    if step == 1:
+
+        if len(supplyDepots) :
+            seed = random.randint(0, len(supplyDepots) - 1)
+            target = chooseARandomPlace(supplyDepots[seed].x, supplyDepots[seed].y)
+            return actions.FunctionCall(actions.FUNCTIONS.Build_Barracks_screen.id, [_QUEUED, target])
+
+    return None
+
+
+def buildSupplydepot(obs, step):
+    # print('buildSupplydepot')
+
+    # actionQueue.put(moveCarmeraToPlayerSelf_atomicOp(obs))
+
+    if not chooseARandomScv_atomicOp(obs):
+        return None
+    if step == 0:
+        return chooseARandomScv_atomicOp(obs)
+
+    if step == 1:
+
+        commandCenters = searchUnitScreen(obs, units.Terran.CommandCenter, 1)
+
+        if len(commandCenters):
+            seed = random.randint(0, len(commandCenters) - 1)
+            target = chooseARandomPlace(commandCenters[seed].x, commandCenters[seed].y)
+            return actions.FunctionCall(actions.FUNCTIONS.Build_SupplyDepot_screen.id, [_QUEUED, target])
+
+    return None
+
+
+def trainMarines(obs, step):
+    # print('trainMarines')
+
+    # 将摄像机随机移动到有己方单位的地方
+    # actionQueue.put(moveCarmeraToPlayerSelf_atomicOp(obs))
+
+    Barracks = searchUnitScreen(obs, units.Terran.Barracks, 1)
+    # 找寻是否当前选择的地方有己方兵营
+    if len(Barracks):
+        ran = random.randint(0, len(Barracks) - 1)
+        target = [Barracks[ran].x, Barracks[ran].y]
+
+        if step == 0:
+            return actions.FunctionCall(actions.FUNCTIONS.select_point.id, [[2], target])
+
+        if step == 1:
+            return actions.FunctionCall(actions.FUNCTIONS.Train_Marine_quick.id, [_NOT_QUEUED])
+
+    else:
+
+        return None
+
+
+def trainSCVs(obs, step):
+    # print('trainSCVs')
+
+    # 将摄像机随机移动到有己方单位的地方
+    # actionQueue.put(moveCarmeraToPlayerSelf_atomicOp(obs))
+
+    CommandCenter = searchUnitScreen(obs, units.Terran.CommandCenter, 1)
+    SCVs = searchUnitScreen(obs, units.Terran.SCV, 1)
+
+    if len(CommandCenter) and len(SCVs) < 16:
+        ran = random.randint(0, len(CommandCenter) - 1)
+        target = [CommandCenter[ran].x, CommandCenter[ran].y]
+        if step == 0:
+            return actions.FunctionCall(actions.FUNCTIONS.select_point.id, [[2], target])
+
+        if step == 1:
+            return actions.FunctionCall(actions.FUNCTIONS.Train_SCV_quick.id, [_NOT_QUEUED])
+
+
+    else:
+        return None
+
+
+def attack(obs, step):
+    # 将摄像机随机移动到有己方单位的地方
+    # actionQueue.put(moveCarmeraToPlayerSelf_atomicOp(obs))
+
+    if obs.observation['player'][8] > 0:
+        if findAnyEnemies(obs):
+            target = findAnyEnemies(obs)
+            if step == 0:
+                return actions.FunctionCall(actions.FUNCTIONS.select_army.id, [[0]])
+            if step == 1:
+                return actions.FunctionCall(actions.FUNCTIONS.Attack_screen.id, [_NOT_QUEUED, target])
+            # actionQueue.put(actions.FUNCTIONS.Attack_screen("now", target))
+        else:
+            # print('attackRandom')
+            random_x = random.randint(0, screenSize - 1)
+            random_y = random.randint(0, screenSize - 1)
+            target = [random_x, random_y]
+            if step == 0:
+                return actions.FunctionCall(actions.FUNCTIONS.select_army.id, [[0]])
+            if step == 1:
+                return actions.FunctionCall(actions.FUNCTIONS.Move_screen.id, [_QUEUED, target])
+
+    return None
---
+++ pysc2/agents/myAgent/myAgent_1.py
@@ -0,0 +1,211 @@
+from queue import Queue
+from absl import app
+
+import pysc2.agents.myAgent.macro_operation as macro_operation
+
+from pysc2.agents import base_agent
+from pysc2.env import sc2_env
+from pysc2.lib import actions
+from pysc2.lib import features
+import pysc2.agents.myAgent.smart_actions as sa
+import pysc2.agents.myAgent.q_learing_table as q_learing_table
+import matplotlib.pyplot as plt
+import numpy as np
+
+_NO_OP = actions.FUNCTIONS.no_op.id
+_NOT_QUEUED = [0]
+_QUEUED = [1]
+
+KILL_UNIT_REWARD = 0.2
+KILL_BUILDING_REWARD = 0.5
+
+
+class myAgent(base_agent.BaseAgent):
+
+    def __init__(self):
+        super(myAgent, self).__init__()
+
+        # 宏操作的大队列
+        self.macroOpQueue = Queue()
+
+        # 宏操作中原子的小队列
+        self.tempMarcoOp = None
+
+        self.tempMarcoOp_step = 0
+
+        self.qlearn = q_learing_table.QLearningTable(actions=list(range(len(sa.smart_actions))))
+
+        self.previous_killed_unit_score = 0
+        self.previous_killed_building_score = 0
+
+        self.previous_action = None
+        self.previous_state = None
+
+        self.figureData = []
+
+    def inQueue(self, functionNumber):
+
+        self.macroOpQueue.put(functionNumber)
+
+        return 0
+
+    def opperation(self, obs):
+
+        # 宏操作队列判空
+        if self.macroOpQueue.empty() and self.tempMarcoOp is None:
+            return actions.FunctionCall(_NO_OP, [])
+
+        if self.tempMarcoOp is None:
+            self.tempMarcoOp = self.macroOpQueue.get()
+
+        if self.tempMarcoOp_step < sa.smart_actions[self.tempMarcoOp][1]:
+
+            atomicOp = sa.smart_actions[self.tempMarcoOp][0](obs, self.tempMarcoOp_step)
+
+            if atomicOp is not None and int(atomicOp[0]) in obs.observation['available_actions']:
+                self.tempMarcoOp_step += 1
+
+                return atomicOp
+
+        self.tempMarcoOp = None
+
+        self.tempMarcoOp_step = 0
+
+        return actions.FunctionCall(_NO_OP, [])
+
+    def step(self, obs):
+        super(myAgent, self).step(obs)
+        current_state = q_learing_table.currentState(obs).getData()
+
+        killed_unit_score = obs.observation['score_cumulative'][5]
+        killed_building_score = obs.observation['score_cumulative'][6]
+
+        self.figureData.append([self.steps, killed_unit_score, killed_building_score])
+
+        if self.previous_action is not None:
+            reward = 0
+
+            if killed_unit_score > self.previous_killed_unit_score:
+                reward += KILL_UNIT_REWARD
+
+            if killed_building_score > self.previous_killed_building_score:
+                reward += KILL_BUILDING_REWARD
+
+            self.qlearn.learn(str(self.previous_state), self.previous_action, reward, str(current_state))
+        rl_action = self.qlearn.choose_action(str(current_state))
+        self.inQueue(rl_action)
+
+        self.previous_killed_unit_score = killed_unit_score
+        self.previous_killed_building_score = killed_building_score
+        self.previous_state = current_state
+        self.previous_action = rl_action
+
+        f = self.opperation(obs)
+
+        return f
+
+
+# def main(unused_argv):
+#     agent = myAgent()
+#     try:
+#         while True:
+#             with sc2_env.SC2Env(
+#                     map_name="Flat96",
+#                     players=[sc2_env.Agent(race=sc2_env.Race.terran, name='agent'),
+#                              sc2_env.Bot(sc2_env.Race.random,
+#                                          sc2_env.Difficulty.very_easy)],
+#                     agent_interface_format=features.AgentInterfaceFormat(
+#                         feature_dimensions=features.Dimensions(screen=macro_operation.screenSize,
+#                                                                minimap=macro_operation.minimapSize),
+#                         camera_width_world_units=macro_operation.screenSize,
+#                         use_unit_counts=True,
+#
+#                     ),
+#
+#                     step_mul=8,
+#                     game_steps_per_episode=0,
+#                     realtime=True,
+#                     visualize=True,
+#
+#             ) as env:
+#
+#                 agent.setup(env.observation_spec(), env.action_spec())
+#                 timesteps = env.reset()
+#                 agent.reset()
+#
+#                 while True:
+#                     step_actions = [agent.step(timesteps[0]),]
+#                     if timesteps[0].last():
+#                         agent.figureData = np.array(agent.figureData)
+#                         plt.plot(agent.figureData[:, 0], agent.figureData[:, 1])
+#                         plt.plot(agent.figureData[:, 0], agent.figureData[:, 2])
+#
+#                         plt.show()
+#
+#                         break
+#                     timesteps = env.step(step_actions, step_mul=0.00000000000000001)
+#
+#     except KeyboardInterrupt:
+#         pass
+#
+#
+# if __name__ == "__main__":
+#     app.run(main)
+
+def main(unused_argv):
+    agent1 = myAgent()
+    agent2 = myAgent()
+
+    try:
+        while True:
+            with sc2_env.SC2Env(
+                    map_name="Flat96",
+                    players=[sc2_env.Agent(race=sc2_env.Race.terran, name='agent1'),
+                             sc2_env.Agent(race=sc2_env.Race.terran, name='agent2'),],
+
+                    agent_interface_format=features.AgentInterfaceFormat(
+                        feature_dimensions=features.Dimensions(screen=macro_operation.screenSize,
+                                                               minimap=macro_operation.minimapSize),
+                        action_space=actions.ActionSpace.RAW,
+                        camera_width_world_units=macro_operation.screenSize,
+                        use_unit_counts=True,
+                        raw_resolution=macro_operation.minimapSize,
+
+                    ),
+
+                    step_mul=8,
+                    game_steps_per_episode=0,
+                    realtime=False,
+                    visualize=False,
+
+            ) as env:
+
+                agent1.setup(env.observation_spec(), env.action_spec())
+                agent2.setup(env.observation_spec(), env.action_spec())
+                timesteps = env.reset()
+                agent1.reset()
+                agent2.reset()
+
+                while True:
+                    step_actions = [agent1.step(timesteps[0]),
+                                    agent2.step(timesteps[1])]
+                    # if timesteps[0].last():
+                    #     agent1.figureData = np.array(agent1.figureData)
+                    #     plt.plot(agent1.figureData[:, 0], agent1.figureData[:, 1])
+                    #     plt.plot(agent1.figureData[:, 0], agent1.figureData[:, 2])
+                    #
+                    #     agent2.figureData = np.array(agent2.figureData)
+                    #     plt.plot(agent2.figureData[:, 0], agent2.figureData[:, 1])
+                    #     plt.plot(agent2.figureData[:, 0], agent2.figureData[:, 2])
+                    #
+                    #     plt.show()
+                    #
+                    #     break
+                    timesteps = env.step(step_actions)
+
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    app.run(main)
