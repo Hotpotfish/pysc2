f1a07e8b849d5ee95d90db4b8771351aefebccce
lenovo <345254314@qq.com>
2019-11-19 22:07:59 +0800

    2019.11.12 封装未完成版

---
+++ pysc2/agents/myAgent/myAgent_6/decisionMaker/DQN.py
@@ -0,0 +1,135 @@
+import datetime
+import os
+from collections import deque
+import random
+
+import numpy as np
+import tensorflow as tf
+
+
+
+from pysc2.agents.myAgent.myAgent_6.net.resNet import resNet
+
+# Hyper Parameters for DQN
+GAMMA = 0.9  # discount factor for target Q
+INITIAL_EPSILON = 0.5  # starting value of epsilon
+FINAL_EPSILON = 0.01  # final value of epsilon
+REPLAY_SIZE = 20000  # experience replay buffer size
+LOOP = 4
+BATCH_SIZE = 32  # size of minibatch
+
+
+class DQN():
+
+    def __init__(self, mu, sigma, learning_rate, actiondim, parameterdim, statedim, name):  # 初始化
+        # 初始化回放缓冲区，用REPLAY_SIZE定义其最大长度
+        self.replay_buffer = deque(maxlen=REPLAY_SIZE)
+
+        # 神经网络参数
+        self.mu = mu
+        self.sigma = sigma
+        self.learning_rate = learning_rate
+
+        self.time_step = 0
+        self.epsilon = INITIAL_EPSILON
+
+        # 动作维度数，动作参数维度数（默认为6）,状态维度数
+        self.action_dim = actiondim
+        self.parameterdim = parameterdim
+        self.state_dim = statedim
+
+        # 网络结构初始化
+        self.name = name
+        self.net = resNet(self.mu, self.sigma, self.learning_rate, self.action_dim, self.parameterdim, self.state_dim,self.name)
+
+
+        self.saver = tf.train.Saver()
+
+        # Init session
+        self.session = tf.InteractiveSession()
+        self.session.run(tf.initialize_all_variables())
+
+        self.restoreModelMark = True
+
+    def restoreModel(self, modelLoadPath):
+        self.saver.restore(self.session, modelLoadPath + '/' + self.name + '.ckpt')
+
+    def perceive(self, state, action, reward, next_state, done):  # 感知存储信息
+        one_hot_action = np.zeros(self.action_dim + self.parameterdim)
+        one_hot_action[int(action[0])] = 1
+        if self.parameterdim != 0:
+            one_hot_action[self.action_dim:] = action[1:]
+        state = np.squeeze(state)
+        next_state = np.squeeze(next_state)
+
+        self.replay_buffer.append([state, one_hot_action, reward, next_state, done])
+
+    def train_Q_network(self, modelSavePath, episode):  # 训练网络
+        # Step 1: obtain random minibatch from replay memory
+
+        if len(self.replay_buffer) > BATCH_SIZE:
+            for mark in range(LOOP):
+                for i in range(BATCH_SIZE):
+                    minibatch = random.sample(self.replay_buffer, BATCH_SIZE)
+                    state_batch = np.array([data[0] for data in minibatch])
+                    action_batch = np.array([data[1] for data in minibatch])
+                    reward_batch = np.array([data[2] for data in minibatch])
+                    next_state_batch = np.array([data[3] for data in minibatch])
+
+                # Step 2: calculate y
+                y_batch = np.array([])
+                Q_value_batch = np.array(self.session.run(self.net.Q_value, {self.net.state_input: next_state_batch}))
+
+                for i in range(0, BATCH_SIZE):
+                    done = minibatch[i][4]
+                    if done:
+                        temp = np.append(np.array(reward_batch[i]), np.array(Q_value_batch[i][self.action_dim:]))
+                        temp = temp.reshape((1, 1 + self.parameterdim))
+                        y_batch = np.append(y_batch, temp)
+                    else:
+                        temp = np.append(np.array(reward_batch[i] + GAMMA * np.max(Q_value_batch[i][0:self.action_dim])),
+                                         Q_value_batch[i][self.action_dim:])
+                        temp = temp.reshape((1, 1 + self.parameterdim))
+                        y_batch = np.append(y_batch, temp)
+                y_batch = np.array(y_batch).reshape(BATCH_SIZE, 1 + self.parameterdim)
+                self.session.run(self.net.train_op,feed_dict={self.net.y_input: y_batch, self.net.action_input: action_batch, self.net.state_input: state_batch})
+        if episode % 2 == 0:
+            self.replay_buffer.clear()
+
+        if episode % 20 == 0:
+            thisPath = modelSavePath + 'episode_' + str(episode) + '/'
+            try:
+                os.makedirs(thisPath)
+            except OSError:
+                pass
+
+            self.saver.save(self.session, thisPath + self.name + '.ckpt', )
+
+    def egreedy_action(self, state):  # 输出带随机的动作
+        Q_value = self.session.run(self.net.Q_value, {self.net.state_input: state})[0]
+        self.epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / 10000
+        if np.random.uniform() <= self.epsilon:
+            # return random.randint(0, self.action_dim - 1)
+            random_action = np.random.randint(0, self.action_dim)
+            random_parameter = np.random.rand(self.parameterdim)
+            random_action_and_parameter = np.append(random_action, random_parameter).flatten()
+            return random_action_and_parameter
+
+        else:
+            action = np.argmax(Q_value[0:self.action_dim])
+            parameter = np.array(Q_value[self.action_dim:(self.action_dim + self.parameterdim)])
+            action_and_parameter = np.append(action, parameter).flatten()
+            return action_and_parameter
+
+    def action(self, state, modelLoadPath):
+
+        if self.restoreModelMark == True and modelLoadPath is not None:
+            self.restoreModelMark = False
+            self.restoreModel(modelLoadPath)
+            print(self.name + 'read!')
+
+        Q_value = self.session.run(self.net.Q_value, {self.net.state_input: state})[0]
+        action = np.argmax(Q_value[0:self.action_dim])
+        parameter = np.array(Q_value[self.action_dim:(self.action_dim + self.parameterdim)])
+        action_and_parameter = np.append(action, parameter)
+        return action_and_parameter
---
+++ pysc2/agents/myAgent/myAgent_6/myAgent_6_full.py
@@ -0,0 +1,63 @@
+from queue import Queue
+
+import pysc2.agents.myAgent.myAgent_4.macro_operation as macro_operation
+
+from absl import app
+from pysc2.agents import base_agent
+from pysc2.agents.myAgent.myAgent_6.decisionMaker.hierarchical_learning_structure import hierarchical_learning_structure
+
+from pysc2.lib import actions, features
+from pysc2.env import sc2_env, run_loop
+
+
+class myAgent(base_agent.BaseAgent):
+
+    def __init__(self):
+        super(myAgent, self).__init__()
+        self.hierarchical_learning_structure = hierarchical_learning_structure()
+
+    def step(self, obs):
+        super(myAgent, self).step(obs)
+        action = self.hierarchical_learning_structure.make_choice(obs, 'TRAIN', 'e:/model/', None)
+        # action = self.hierarchical_learning_structure.make_choice(obs, 'TRAIN', 'e:/model/', 'model/20191118211813/episode_20')
+        # action = self.hierarchical_learning_structure.make_choice(obs, 'TEST', None, 'e:/model/20191119112956/episode_0')
+        # action = self.hierarchical_learning_structure.make_choice(obs, 'TEST', None, None)
+
+        return action
+
+
+def main(unused_argv):
+    agent1 = myAgent()
+
+    try:
+        with sc2_env.SC2Env(
+                map_name="Flat96",
+                players=[sc2_env.Agent(sc2_env.Race.terran),
+                         sc2_env.Bot(sc2_env.Race.protoss,
+                                     sc2_env.Difficulty.very_easy)],
+                agent_interface_format=features.AgentInterfaceFormat(
+                    feature_dimensions=features.Dimensions(screen=macro_operation.mapSzie,
+                                                           minimap=macro_operation.mapSzie),
+                    camera_width_world_units=macro_operation.mapSzie * 1,
+
+                    action_space=actions.ActionSpace.RAW,
+                    use_raw_units=True,
+                    raw_resolution=macro_operation.mapSzie,
+                    use_unit_counts=True
+                ),
+                score_index = -1,
+
+                step_mul=16,
+                disable_fog=False,
+                visualize=True,
+                realtime=False
+
+        ) as env:
+            run_loop.run_loop([agent1], env)
+
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    app.run(main)
