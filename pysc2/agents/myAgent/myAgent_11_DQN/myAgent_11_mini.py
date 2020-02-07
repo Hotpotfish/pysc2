908c3b42955a503bd7037e8e230b7df152cc7905
BuildTools <unconfigured@null.spigotmc.org>
2020-02-07 14:11:06 +0800

    2.5 大杂烩（存档点）

--- pysc2/agents/myAgent/myAgent_11_DQN/myAgent_11_mini.py
+++ pysc2/agents/myAgent/myAgent_11_DQN/myAgent_11_mini.py
@@ -24,6 +24,7 @@ class myAgent(base_agent.BaseAgent):
 
     def step(self, obs):
         # self.add_or_plt(obs, self.steps)
+
         super(myAgent, self).step(obs)
         # action = self.hierarchical_learning_structure.execute(obs, 'TRAIN', 'd:/model/', None)
         # action = self.hierarchical_learning_structure.execute(obs, 'TRAIN', 'e:/model/',  'D:/model/20191230172144/episode_300')
