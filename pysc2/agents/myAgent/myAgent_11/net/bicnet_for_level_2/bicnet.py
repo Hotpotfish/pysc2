294a6cdeb506df5cb4ad74863ce8150f59fee817
BuildTools <unconfigured@null.spigotmc.org>
2020-02-14 00:43:25 +0800

    2.14 最近工作存档版

--- pysc2/agents/myAgent/myAgent_11/net/bicnet_for_level_2/bicnet.py
+++ pysc2/agents/myAgent/myAgent_11/net/bicnet_for_level_2/bicnet.py
@@ -21,23 +21,8 @@ class bicnet(object):
         self.name = name
 
         self._setup_placeholders_graph()
-        with tf.variable_scope('Actor'):
-            self.a = self._build_graph_a(self.agents_local_observation, 'eval', train=True)
-            a_ = self._build_graph_a(self.agents_local_observation_next, 'target', train=False)
 
-        with tf.variable_scope('Critic'):
-            self.q = self._build_graph_c(self.state_input, self.a, 'eval', train=True)
-            q_ = self._build_graph_c(self.state_input_next, a_, 'target', train=False)
-
-        # networks parameters
-        self.ae_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/eval')
-        self.at_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/target')
-        self.ce_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval')
-        self.ct_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target')
-
-        # target net replacement
-        self.soft_replace = [tf.assign(t, (1 - config.TAU) * t + config.TAU * e)
-                             for t, e in zip(self.at_params + self.ct_params, self.ae_params + self.ce_params)]
+        self.q = self._build_graph_q(self.state_input, self.agents_local_observation, 'q_value', train=True)
 
         q_target = self.reward + config.GAMMA * q_
 
@@ -62,55 +47,13 @@ class bicnet(object):
 
         self.reward = tf.placeholder("float", shape=[None], name='reward')
 
-    def _build_graph_a(self, agents_local_observation, scope_name, train):
+
+    def _build_graph_q(self, state_input, action_input, scope_name, train):
         # 环境和智能体本地的共同观察
         with tf.variable_scope(scope_name):
             with slim.arg_scope([slim.conv2d, slim.fully_connected],
                                 trainable=train,
-                                activation_fn=tf.nn.relu,
-                                normalizer_fn=slim.batch_norm,
-                                weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
-                                weights_regularizer=slim.l2_regularizer(0.05)):
-                encoder_outputs = self._observation_encoder_a(agents_local_observation, self.agents_number, '_observation_encoder')
-                bicnet_outputs = self._bicnet_build_a(encoder_outputs, self.agents_number, '_bicnet_build')
-                return bicnet_outputs
-
-    def _observation_encoder_a(self, agents_local_observation, agents_number, scope_name):
-        with tf.variable_scope(scope_name):
-            encoder = []
-            for i in range(agents_number):
-                fc1 = slim.fully_connected(agents_local_observation[:, i, :], 100, scope='full_connected1' + "_" + str(i))
-                encoder.append(fc1)
-            encoder = tf.transpose(encoder, [1, 0, 2])
-            encoder = tf.unstack(encoder, agents_number, 1)  # (self.agents_number,batch_size,obs_add_dim)
-            return encoder
-
-    def _bicnet_build_a(self, encoder_outputs, agents_number, scope_name):
-        with tf.variable_scope(scope_name):
-            outputs = []
-            lstm_fw_cell = tf.nn.rnn_cell.GRUCell(self.action_dim, name="lstm_fw_cell")
-            lstm_bw_cell = tf.nn.rnn_cell.GRUCell(self.action_dim, name="lstm_bw_cell")
-            bicnet_outputs, _, _ = tf.nn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, encoder_outputs,
-                                                                  dtype=tf.float32)
-            for i in range(agents_number):
-                # fc1 = slim.fully_connected(bicnet_outputs[i], 30, scope='full_connected1' + "_" + str(i))
-                fc1 = slim.fully_connected(bicnet_outputs[i], 1, activation_fn=tf.nn.tanh,
-                                           scope='full_connected1' + "_" + str(i))
-                fc1 = fc1 * self.action_dim
-                outputs.append(fc1)
-
-            outputs = tf.unstack(outputs, self.agents_number)  # (agents_number, batch_size, action_dim)
-            outputs = tf.transpose(outputs, [1, 0, 2])
-            return outputs  # (batch_size,agents_number,action_dim)
-
-        #################################### critic_net  ####################################
-
-    def _build_graph_c(self, state_input, action_input, scope_name, train):
-        # 环境和智能体本地的共同观察
-        with tf.variable_scope(scope_name):
-            with slim.arg_scope([slim.conv2d, slim.fully_connected],
-                                trainable=train,
-                                activation_fn=tf.nn.relu,
+                                activation_fn=tf.nn.selu,
                                 normalizer_fn=slim.batch_norm,
                                 weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                                 weights_regularizer=slim.l2_regularizer(0.05)):
@@ -119,7 +62,7 @@ class bicnet(object):
                 bicnet_outputs = self._bicnet_build_c(encoder_outputs, self.agents_number, '_bicnet_build')
                 return bicnet_outputs
 
-    def _observation_encoder_c(self, state_input, action_input, agents_number, scope_name):
+    def _observation_encoder_q(self, state_input, action_input, agents_number, scope_name):
         with tf.variable_scope(scope_name):
             encoder = []
             for i in range(agents_number):
@@ -134,7 +77,7 @@ class bicnet(object):
             encoder = tf.unstack(encoder, agents_number, 1)  # (self.agents_number,batch_size,obs_add_dim)
             return encoder
 
-    def _bicnet_build_c(self, encoder_outputs, agents_number, scope_name):
+    def _bicnet_build_q(self, encoder_outputs, agents_number, scope_name):
         with tf.variable_scope(scope_name):
             outputs = []
             lstm_fw_cell = tf.nn.rnn_cell.GRUCell(self.action_dim, name="lstm_fw_cell")
@@ -147,7 +90,7 @@ class bicnet(object):
             outputs = tf.unstack(outputs, self.agents_number)  # (agents_number, batch_size,1)
             outputs = tf.transpose(outputs, [1, 0, 2])  # (batch_size,agents_number,1)
             outputs = slim.flatten(outputs)
-            fc2 = slim.fully_connected(outputs, 1, activation_fn=None,scope='full_connected2')
+            fc2 = slim.fully_connected(outputs, 1, activation_fn=None, scope='full_connected2')
             # fc3 = slim.fully_connected(fc2, 1, scope='full_connected3')
 
             return fc2
